d185a19e7d16869eb2df0277a8ec37dd8ccf0534
Jay Prajapati <jay.prajapati5717@gmail.com>
2025-03-05 00:13:28 +0530

    Fix: case change for import file name reference

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
-import Login from './pages/login';
+import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import EditorPage from './pages/EditorPage';
 import ProtectedRoute from './components/ProtectedRoute';
